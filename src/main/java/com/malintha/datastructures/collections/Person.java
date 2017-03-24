74bd1d708d1adc8d1d006c11a64a3008b5ccb10f
malintha <malinthasa@gmail.com>
2017-03-24 13:23:59 +0530

    initial commit

---
+++ src/main/java/com/malintha/datastructures/collections/Person.java
@@ -0,0 +1,16 @@
+package com.malintha.datastructures.collections;
+
+/**
+ * Created by malintha on 3/24/17.
+ */
+public class Person {
+
+    private String name;
+    private String number;
+
+    public Person(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+}
---
+++ src/main/java/com/malintha/datastructures/collections/TestHashMap.java
@@ -0,0 +1,13 @@
+package com.malintha.datastructures.collections;
+
+import java.util.HashMap;
+
+/**
+ * Created by malintha on 3/24/17.
+ */
+public class TestHashMap {
+
+    public static void main(String[] args) {
+        HashMap<String,String> myHashMap = new HashMap<String, String>();
+    }
+}
